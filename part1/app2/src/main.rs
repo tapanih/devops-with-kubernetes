b4f01c5bd2c2b2b7cae6c0c03235568b9865a385
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-12-25 22:23:56 +0200

    Exercise 1.05

--- part1/app2/src/main.rs
+++ part1/app2/src/main.rs
@@ -1,7 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-extern crate rocket;
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
 fn main() {
-    rocket::ignite().launch();
+    rocket::ignite().mount("/", routes![index]).launch();
 }
