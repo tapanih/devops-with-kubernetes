dd9fcded231e8df2706468824b87f21a8a9f6110
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-01-02 13:43:12 +0200

    Exercise 2.07

--- pingpong/src/main.rs
+++ pingpong/src/main.rs
@@ -1,33 +1,50 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate lazy_static;
 
-use std::sync::atomic::{AtomicUsize, Ordering};
-use rocket::State;
-use std::env;
-use std::fs;
+use postgres::{Client, NoTls};
 
-struct Counter(AtomicUsize);
+lazy_static! {
+    static ref POSTGRES_URL: String = std::env::var("POSTGRES_URL")
+        .expect("POSTGRES_URL not found");
+}
+
+fn initialize_database() {
+    let mut client: Client = Client::connect(&POSTGRES_URL[..], NoTls)
+        .expect("could not connect to database");
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS pong ( ping INTEGER );
+        INSERT INTO pong (ping) VALUES (0);
+        CREATE OR REPLACE RULE no_insert AS ON INSERT TO pong DO INSTEAD NOTHING;
+    ").unwrap();
+}
+
+fn get_pongs_from_db(client: &mut Client) -> i32 {
+    return match client.query_one("SELECT ping FROM pong", &[]) {
+        Ok(row) => row.get("ping"),
+        Err(_) => 0,
+    };
+}
 
 #[get("/pongs")]
-fn pong(counter: State<Counter>) -> String {
-    format!("{}", counter.0.load(Ordering::Relaxed))
+fn pong() -> String {
+    let mut client: Client = Client::connect(&POSTGRES_URL[..], NoTls)
+        .expect("could not connect to database");
+    format!("{}", get_pongs_from_db(&mut client))
 }
 
 #[get("/pingpong")]
-fn index(counter: State<Counter>) -> String {
-    let count = counter.0.fetch_add(1, Ordering::Relaxed);
-    let filename = env::var("PINGPONG_FILE")
-        .expect("PINGPONG_FILE not found");
-    fs::write(&filename, count.to_string())
-        .unwrap_or_else(|_| {
-            println!("Could not write to file.");
-        });
-    format!("pong {}", count.to_string())
+fn index() -> String {
+    let mut client: Client = Client::connect(&POSTGRES_URL[..], NoTls)
+        .expect("could not connect to database");
+    client.batch_execute("UPDATE pong SET ping = ping + 1;").ok();
+    format!("pong {}", get_pongs_from_db(&mut client))
 }
 
 fn main() {
+    initialize_database();
     rocket::ignite()
       .mount("/", routes![index, pong])
-      .manage(Counter(AtomicUsize::new(0))).launch();
+      .launch();
 }
